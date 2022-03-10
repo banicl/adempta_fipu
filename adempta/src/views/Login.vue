0de225cdbb3ac2d8b9a3e904a0e9232fda8ada9b
Leonarda Banić <banic.leonarda2@gmail.com>
2022-03-10 16:06:25 +0100

    adempta upload 2

---
+++ adempta/src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="login">
+    <h1>Login</h1>
+    <input type="text" name="email" v-model="inputemail" placeholder="Email" />
+    <input
+      type="lozinka"
+      name="lozinka"
+      v-model="inputlozinka"
+      placeholder="Lozinka"
+    />
+    <button type="button" v-on:click="login()">Login</button>
+  </div>
+</template>
+
+<script>
+import firebase from "@/firebase";
+export default {
+  name: "login",
+  data() {
+    return {
+      inputemail: "",
+      inputlozinka: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("login..." + this.inputemail);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.inputemail, this.inputlozinka)
+        .then(function (result) {
+          console.log("Uspjesna prijava", result);
+        })
+        .catch(function (e) {
+          //u slucaju greske catch prima jednu funkciu u koju stvlja eror objekt
+          console.error("greska", e);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  width: 500px;
+  border: 1px solid #cccccc;
+  background-color: #ffffff;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
+</style>
