6e5977d952958f5093975c03d6fba62e1328d956
MartaTilman <75136778+MartaTilman@users.noreply.github.com>
2022-03-22 19:50:24 +0100

    odabir_dana,odabir_admin,inf_admin

--- adempta/src/router/index.js
+++ adempta/src/router/index.js
@@ -25,6 +25,46 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "Login" */ '../views/SignUp.vue')
+  },
+  {
+    path: '/admin_inf',
+    name: 'Admin_inf',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Admin_inf" */ '../views/Admin_inf.vue')
+  },
+  {
+    path: '/odabir_rada',
+    name: 'Odabir_rada',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Odabir_rada_admin" */ '../views/Odabir_rada_admin.vue')
+  },
+  {
+    path: '/uredi',
+    name: 'Uredi_liniju',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Odabir_rada_admin" */ '../views/Uredi_liniju.vue')
+  },
+  {
+    path: '/prikaz',
+    name: 'Prikaz_linija',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Odabir_rada_admin" */ '../views/Prikaz_linija.vue')
+  },
+  {
+    path: '/odabir_dana',
+    name: 'Odabir_dana',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "Odabir_rada_admin" */ '../views/Odabir_dana.vue')
   }
 ]
 
